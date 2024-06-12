4c3373e44ec82aac89663b889b392f6bb3e86bbd
Tom <tom.sander243@gmail.com>
2024-06-12 12:15:39 +0200

    pictures on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import berlinImage from '../imgs/berlin.jpg';
+import singaporeImage from '../imgs/singapore.jpg';
+import losAngelesImage from '../imgs/los-angeles.jpg';
 
 export default function Home() {
-  return ("Home page works");
+  return(
+      <div>
+      <Image src={berlinImage} width={500} height={500} alt="Berlin"></Image>,
+    <Image src={singaporeImage} width={500} height={500} alt="Singapore"></Image>,
+    <Image src={losAngelesImage} width={500} height={500} alt="Los-Angeles"></Image>
+      </div>
+  );
 }
